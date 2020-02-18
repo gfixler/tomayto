977d98ae07ad25d4ddea408ab04fbda7153b2afc
gfixler <gary@garyfixler.com>
2020-02-17 21:16:28 -0800

    Rename defaultCB to tester
    It's not going to be a default soon.

--- core.py
+++ core.py
@@ -5,13 +5,12 @@ from . import util
 
 class Tomayto (object):
 
-    def defaultCB (self, key, alt, ctrl, press):
+    def tester (self, key, alt, ctrl, press):
         """
-        This default callback simply prints out the pressed and released keys,
-        and modified variants, when assigned to the global callback name. It's
-        only intended to check that the nameCommands and hotkeys have been set
-        up correctly; normal use is to assign another handler to the global
-        callback name.
+        When assigned to the global callback name, this callback prints out
+        pressed and released keys, and modified variants. It's useful to check
+        that the nameCommands and hotkeys have been set up correctly; normal
+        use is to assign another handler to the global callback name.
         """
         if press:
             print "pressed: " + ("alt + " if alt else "") + ("ctrl + " if ctrl else "") + util.charName(key)
