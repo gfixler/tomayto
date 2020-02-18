e3d34218ab0b68deb0f094dc1aa4b25d9269cf11
gfixler <gary@garyfixler.com>
2020-02-18 04:02:00 -0800

    Begin Tomayto init method
    I think this is the right start to where I'm heading with this.

--- core.py
+++ core.py
@@ -5,6 +5,16 @@ from . import util
 
 class Tomayto (object):
 
+    def __init__ (self, states, startState):
+        """
+        Each state must be a class with an init that accepts a handler func.
+        "states" is a dict of name:state pairs of state class references.
+        "startState" is the (string) key of the starting state in states.
+        """
+        self.states = states
+        self.startState = states[startState](self) # init start state
+        self.stateStack = [] # start state doesn't go on stack/can't be popped
+
     def tester (self, key, alt, ctrl, press):
         """
         When assigned to the global callback name, this callback prints out
