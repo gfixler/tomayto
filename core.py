a5bce296c8ef3fbe1c4b31afdae23b5227e093f1
gfixler <gary@garyfixler.com>
2020-02-18 19:24:33 -0800

    Reword, rename, rework contents of init method

--- core.py
+++ core.py
@@ -5,14 +5,17 @@ import util
 
 class Tomayto (object):
 
-    def __init__ (self, states, startState):
+    def __init__ (self, statesMap, startStateName):
         """
-        Each state must be a class with an init that accepts a handler func.
-        "states" is a dict of name:state pairs of state class references.
-        "startState" is the (string) key of the starting state in states.
+        Each state must be a class with an init that accepts a reference to the
+        instance of this class that instantiates it. Each instance should store
+        the ref for use in communicating with it later.
+        "statesMap" is a dict of name:state pairs of state class references.
+        "startStateName" is the string key of the starting state in statesMap.
         """
-        self.states = states
-        self.startState = states[startState](self) # init start state
+        self.statesMap = statesMap
+        self.startStateName = startStateName
+        self.startState = statesMap[startStateName](self) # init start state
         self.stateStack = [] # start state doesn't go on stack/can't be popped
 
     def tester (self, key, alt, ctrl, press):
