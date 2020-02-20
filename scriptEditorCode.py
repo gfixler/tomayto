fd544ba20ad44346e4cdf2ef0056854370f507a0
gfixler <gary@garyfixler.com>
2020-02-20 10:19:17 -0800

    Clean up scriptEditorCode.py
    Remove unneeded things, improve existing stuff.

--- scriptEditorCode.py
+++ scriptEditorCode.py
@@ -2,16 +2,6 @@
 # but still allow stream selecting lines between and running
 
 """
-cmds.hotkeySet(query=True, hotkeySetArray=True)
-cmds.hotkeySet(query=True, current=True)
-
-cmds.hotkeySet("MyNewKeySet", current=True) # creates if non-existent
-
-cmds.hotkeySet("Maya_Default", edit=True, current=True)
-cmds.hotkeySet("MyNewKeySet", edit=True, current=True)
-"""
-
-"""
 import sys
 
 path = "C:/msys64/home/GFixler/code/py"
@@ -19,31 +9,28 @@ if path not in sys.path:
     sys.path = [path] + sys.path
 """
 
-del sys.modules["tomayto"]
-del sys.modules["tomayto.core"]
-del sys.modules["tomayto.util"]
+try:
+    del tomayto
+except:
+    pass
+mods = sys.modules.keys()
+for mod in mods:
+    if mod.startswith("tomayto"):
+        del sys.modules[mod]
+        print "deleted", mod
 
 import tomayto
-reload(tomayto)
-reload(tomayto.core)
-reload(tomayto.util)
+
+try:
+    reload(tomayto)
+    reload(tomayto.core)
+    reload(tomayto.util)
+    reload(tomayto.exampleUsage)
+except:
+    pass
 
 # create our instance
-tom = tomayto.Tomayto()
-
-"""
-# should only need to create once
-tomayto.createTomaytoKeymap()
-tomayto.removeTomaytoKeymap()
-"""
-
-# tomayto.util.listAllNameCommands()
-
+tom = tomayto.exampleUsage.instantiate()
 # hook up the Python callback (called from MEL) to our instance
-tomaytoCB = tom.defaultCB
-
-"""
-def tomaytoCB (key, alt, ctrl, press):
-    print key, "Alt" if alt else "", "Ctrl" if ctrl else "", "DOWN" if press else "UP"
-"""
+tomaytoCB = tom.eventHandler
 
