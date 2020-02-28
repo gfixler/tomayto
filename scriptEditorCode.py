0344a41bcb9c1cfd84d6afe78be541b0dd1f1c8f
gfixler <gary@garyfixler.com>
2020-02-27 18:25:03 -0800

    Remove most of scriptEditorCode.py
    I came up with a shelf button launched UI that handles cleanly reloading
    things, at least under my workflow, so i don't need all of this. Also,
    I've just made sure my needed paths are in my userSetup.py, so that also
    isn't needed now. I'm now running a tighter ship.

--- scriptEditorCode.py
+++ scriptEditorCode.py
@@ -1,34 +1,3 @@
-# triple-quotes to comment out against select-all->run
-# but still allow stream selecting lines between and running
-
-"""
-import sys
-
-path = "C:/msys64/home/GFixler/code/py"
-if path not in sys.path:
-    sys.path = [path] + sys.path
-"""
-
-try:
-    del tomayto
-except:
-    pass
-mods = sys.modules.keys()
-for mod in mods:
-    if mod.startswith("tomayto"):
-        del sys.modules[mod]
-        print "deleted", mod
-
-import tomayto
-
-try:
-    reload(tomayto)
-    reload(tomayto.core)
-    reload(tomayto.util)
-    reload(tomayto.exampleUsage)
-except:
-    pass
-
 # create our instance
 tom = tomayto.exampleUsage.instantiate()
 # hook up the Python callback (called from MEL) to our instance
