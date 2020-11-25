8a72e7548cf09b5053f6710a97d131000540ae8f
gfixler <gary@garyfixler.com>
2020-11-25 08:01:57 -0800

    Remove Tomayto tester method
    I initially used this to test that the keys callbacks were happening, by
    assigning it to the global callback. Now I always assign the handler
    method to the global callback, and haven't used this in quite a while.

--- core.py
+++ core.py
@@ -78,19 +78,6 @@ class Tomayto (object):
         for c, i in self.stateStack:
             print "\t", c.__name__, i
 
-    def tester (self, key, alt, ctrl, press):
-        """
-        When assigned to the global callback name, this callback prints out
-        pressed and released keys, and modified variants. It's useful to check
-        that the nameCommands and hotkeys have been set up correctly; normal
-        use is to assign another handler to the global callback name.
-        """
-        if press:
-            print "pressed: " + ("alt + " if alt else "") + ("ctrl + " if ctrl else "") + util.keyName(key)
-        else:
-            print "released: " + ("alt + " if alt else "") + ("ctrl + " if ctrl else "") + util.keyName(key)
-
-
 
 def enable (hotkeySetName="Tomayto", **kwargs):
     try:
