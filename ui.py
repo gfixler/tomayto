9a27089786b26aff95989174ed175144061bbb2e
gfixler <gary@garyfixler.com>
2020-12-31 23:11:00 -0800

    Remove underscores in SelectionList vars

--- ui.py
+++ ui.py
@@ -23,60 +23,60 @@ SelectionListDefaults = {
 class SelectionList (object):
 
     def __init__ (self, values=[], createUI=True, settings={}):
-        self._values = values
-        self._entries = []
-        self._settings = settings
+        self.values = values
+        self.entries = []
+        self.settings = settings
         if createUI:
             self.createUI()
 
     def clearUI (self):
-        for entry in self._entries:
+        for entry in self.entries:
             cmds.deleteUI(entry)
-        self._entries = []
+        self.entries = []
 
     def populateUI (self):
         self.clearUI()
-        for value in self._values:
+        for value in self.values:
             entry = cmds.text( label = value
-                             , parent = self._form
-                             , align = self._settings["textAlign"]
-                             , font = self._settings["font"]
-                             , backgroundColor = self._settings["bgCol"]
+                             , parent = self.form
+                             , align = self.settings["textAlign"]
+                             , font = self.settings["font"]
+                             , backgroundColor = self.settings["bgCol"]
                              )
-            if self._settings["fullWidthSelection"]:
-                cmds.formLayout(self._form, edit=True, attachForm=[(entry, "left", 0), (entry, "right", 0)])
-            self._entries.append(entry)
-        if self._entries:
-            for top, bot in zip(self._entries, self._entries[1:]):
-                cmds.formLayout(self._form, edit=True, attachControl=[(bot, "top", 0, top)])
+            if self.settings["fullWidthSelection"]:
+                cmds.formLayout(self.form, edit=True, attachForm=[(entry, "left", 0), (entry, "right", 0)])
+            self.entries.append(entry)
+        if self.entries:
+            for top, bot in zip(self.entries, self.entries[1:]):
+                cmds.formLayout(self.form, edit=True, attachControl=[(bot, "top", 0, top)])
         cmds.evalDeferred(self.extractTextHeight)
 
     def extractTextHeight (self):
-        text = self._entries[0]
+        text = self.entries[0]
         self.textHeight = cmds.text(text, query=True, height=True)
 
     def scrollPage (self, direction="down"):
-        saHeight = cmds.scrollLayout(self._scroll, query=True, height=True)
+        saHeight = cmds.scrollLayout(self.scroll, query=True, height=True)
         pixels = saHeight / self.textHeight * self.textHeight
-        cmds.scrollLayout(self._scroll, edit=True, scrollByPixel=(direction, pixels))
+        cmds.scrollLayout(self.scroll, edit=True, scrollByPixel=(direction, pixels))
 
     def scrollHalfPage (self, direction="down"):
-        saHeight = cmds.scrollLayout(self._scroll, query=True, height=True)
+        saHeight = cmds.scrollLayout(self.scroll, query=True, height=True)
         pixels = saHeight / self.textHeight / 2 * self.textHeight
-        cmds.scrollLayout(self._scroll, edit=True, scrollByPixel=(direction, pixels))
+        cmds.scrollLayout(self.scroll, edit=True, scrollByPixel=(direction, pixels))
 
     def highlightIndex (self, n):
-        if n >= 0 and n < len(self._entries):
-            cmds.text(self._entries[n], edit=True, backgroundColor=self._settings["hlCol"])
+        if n >= 0 and n < len(self.entries):
+            cmds.text(self.entries[n], edit=True, backgroundColor=self.settings["hlCol"])
 
     def clearHighlightIndex (self, n):
-        if n >= 0 and n < len(self._entries):
-            cmds.text(self._entries[n], edit=True, backgroundColor=self._settings["bgCol"])
+        if n >= 0 and n < len(self.entries):
+            cmds.text(self.entries[n], edit=True, backgroundColor=self.settings["bgCol"])
 
     def createUI (self):
-        self._scroll = cmds.scrollLayout( childResizable = self._settings["fullWidthSelection"]
-                                        , backgroundColor = self._settings["bgCol"]
+        self.scroll = cmds.scrollLayout( childResizable = self.settings["fullWidthSelection"]
+                                        , backgroundColor = self.settings["bgCol"]
                                         )
-        self._form = cmds.formLayout()
+        self.form = cmds.formLayout()
         self.populateUI()
 
